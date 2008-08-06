28e96894658f2f71b303430ccd15d4e48713076e
David Baldwin <baldwindavid@gmail.com>
2008-08-06 15:24:41 -0400

    added revert_models_to_utc method for migration down states

--- lib/bilson/models_to_utc.rb
+++ lib/bilson/models_to_utc.rb
@@ -7,18 +7,27 @@ module Bilson
     
     module ClassMethods
       
-      def models_to_utc(models, from_time_zone = '')
+      def models_to_utc(models, from_time_zone, revert = false)
         models = models.to_a
         models.each do |model|    
-          model.find(:all).each do |record|
+          model.all.each do |record|
             timestamps = {}        
             model.timestamp_columns.each do |column|
               rc = record.send(column.name)
-              timestamps[column.name] = !rc.blank? ? rc.utc - rc.in_time_zone(from_time_zone).utc_offset : rc
+              if rc.blank?
+                timestamps[column.name] = rc
+              else
+                add_or_subtract = revert == true ? '+' : '-'  
+                timestamps[column.name] = rc.utc.send(add_or_subtract, rc.in_time_zone(from_time_zone).utc_offset)
+              end
             end
             record.update_attributes(timestamps)
           end
         end
+      end
+      
+      def revert_models_to_utc(models, from_time_zone)
+        models_to_utc(models, from_time_zone, true)
       end
       
     end
